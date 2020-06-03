86359a0bfdd7bda2cbe1d215264d9d868c2f027b
GerardoQ <gerardoquispe65@gmail.com>
2020-06-03 00:07:56 -0500

    modifiqued landing

--- src/components/Carrousel/Carrusel.js
+++ src/components/Carrousel/Carrusel.js
@@ -1,5 +1,5 @@
 import React, { useState,useEffect } from 'react'
-import './carrusel.scss'
+
 import ItemCarrusel from './components/item.js'
 
 import Background from './../Carrousel/slide.png';
--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -6,10 +6,9 @@ const Menu = () => {
     const [categoria, setCategoria] = useState([
         {id: 1, nombre: "Moda Hombre"}, 
         {id: 2, nombre: "Moda Mujer"}, 
-        {id: 3, nombre: "Zapatilla"}, 
+        {id: 3, nombre: "Moda Niños"}, 
         {id: 4, nombre: "Accesorio"}, 
-        {id: 5, nombre: "Maquina"},
-        {id: 6, nombre: "Oferta"}
+        {id: 5, nombre: "Maquina"}
       ]);
       return (
                   <ul className="menu">
