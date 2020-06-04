2286d8dae138936dc82eb253f67b73c1c15ef0ad
Jonathang.espinoza <jonathang.espinoza@gmail.com>
2020-06-03 19:14:59 -0600

    Products Related

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router,Route, Switch } from 'react-router-dom';
 import Landing from './views/Landing/index'
 import Categoria from './views/Categoria/index'
 import Register from './views/Register/index'
+import Products from './views/Products/index'
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
          <Route path="/" component={Landing} exact></Route>
          <Route path="/categoria/:id" component={Categoria} exact></Route>
          <Route path="/register" component={Register} exact></Route>
+         <Route path="/products" component={Products} exact></Route>
        </Switch>
      </Router>
 
---
+++ src/views/Products/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ProductsRelated from '../../components/ProductsRelated/index'
+
+const Products = () => {
+    return ( 
+        <div>
+            <ProductsRelated />
+        </div> 
+    );
+}
+ 
+export default Products;
